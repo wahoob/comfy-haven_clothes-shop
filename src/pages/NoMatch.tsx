7741718a11ec1b8efbe7e8fa5209f4bf9841d6fd
wahoob <majed.eddin.wahoub@gmail.com>
2024-03-21 05:28:19 +0200

    edditing no match page

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -25,7 +25,7 @@ const NoMatch = () => {
             .to(
                 manRef.current,
                 {
-                    rotate: 2,
+                    transform: "translateX(-47%) rotate(2deg)",
                     duration: 5,
                     ease: "sine.out",
                 },
@@ -101,7 +101,7 @@ const NoMatch = () => {
                 <img
                     src={manfloat}
                     alt="man float"
-                    className="absolute bottom-0 max-w-72 lg:max-w-96 left-1/2 -translate-x-1/2 -rotate-2"
+                    className="absolute bottom-0 max-w-72 lg:max-w-96 left-1/2 -translate-x-[53%] -rotate-2"
                     ref={manRef}
                 />
                 <img
